51b9a15450a6d4589bb2bad5dc2b7b8fad666dca
estherviolin <esthernaomi.violin@gmail.com>
2020-11-02 15:58:23 -0600

    app views, nav bar, react-router-dom installed

---
+++ src/components/raterproject.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import "./raterproject.css"
+
+
+export const RaterProject = () => (
+    <>
+        <Route render={() => {
+            if (localStorage.getItem("rg_token")) {
+                return <>
+                    <Route render={NavBar} />
+                    <Route render={props => <ApplicationViews {...props} />} />
+                </>
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={Login} />
+        <Route path="/register" render={Register} />
+    </>
+)
